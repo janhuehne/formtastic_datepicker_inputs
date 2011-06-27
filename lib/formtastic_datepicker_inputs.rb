f45a2ba0c8c68d469d2b4fe49e83921cc0fe4be5
Jan-Hendrik Hühne <jan.huehne@googlemail.com>
2011-06-27 14:29:07 +0200

    Correct hash for newest formtastic plugin

--- lib/formtastic_datepicker_inputs.rb
+++ lib/formtastic_datepicker_inputs.rb
@@ -22,8 +22,10 @@ module Formtastic
       inp_opts = options.delete(:input_html) || {}
       clss << inp_opts.delete(:class).to_s
       clss << options.delete(:class).to_s
-      options[:input_html] = {:class => "ui-date-picker " + clss.join(' ')}.merge inp_opts
-      options[:value] = options[:value] || object.send(method).try(:strftime, format)
+      options[:input_html] = {
+        :class => "ui-date-picker " + clss.join(' '),
+        :value => (options[:value] || object.send(method).try(:strftime, format))
+      }.merge inp_opts
       basic_input_helper(:text_field, :string, method, options)
     end
   end
